bc848587b0673167e23b7c13072dd97400451bc0
Kazutaka Ashizawa <k.ashizawa@gmail.com>
2015-02-10 02:28:29 +0900

    change depth path

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ module.exports = function (opt) {
     try {
       var filePath = file.base;
       var fileCwd = file.cwd;
-      var fileName = gutil.replaceExtension(path.basename(file.path), "");
+      var fileName = gutil.replaceExtension(path.relative(file.base, file.path), "");
 
       var relativeBase = path.relative(fileCwd, filePath);
       var relativePath = path.join(relativeBase, fileName);
